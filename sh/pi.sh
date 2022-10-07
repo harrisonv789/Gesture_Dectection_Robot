0f76e7415c04181068d3af301a997be30d4c43cf
harrisonv789 <harrison.v1@live.com>
2022-10-07 11:19:41 +1100

    Added in the PI starting script.

---
+++ sh/pi.sh
@@ -0,0 +1,22 @@
+#/bin/bash
+
+echo "Running the executable file for the PI."
+
+# Change the root directory
+cd "/home/pi/Desktop/Sign_Language_Neural/"
+
+# Start the streaming service
+./src/cameras/gst_source.sh &
+
+# Run the main file
+cd "src/controller/"
+./main.py
+
+# Retrieve the process ID of the camera and kill service
+pid=$(pidof gst-launch-1.0)
+sudo kill -9 $pid
+
+# Disable all lights
+python3 led.py 0
+
+echo "Completed the execution file of the PI."
