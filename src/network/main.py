6e682aa0a48a3feddc555e2471be6c397963ebe0
Harrison Verrios <harrison.v1@live.com>
2022-10-09 13:10:14 +1100

    Updated the main network script to work with opencv.

--- src/network/main.py
+++ src/network/main.py
@@ -1,6 +1,6 @@
 #!/bin/python3
 
-import time, os, socket
+import time, os, socket, sys
 from image import Image
 from model import Model
 from gesture import Gesture
@@ -11,6 +11,14 @@ def main (ip: str = "192.168.1.112", use_cv: bool = False):
     This is the main function that is executed to check for 
     image and then process the results
     '''
+    
+    # Print out the current state of the result
+    if use_cv:
+        print("Using an OpenCV neural network for the sign language.")
+        data_length: int = 5
+    else:
+        print("Using a trained neural network for letters.")
+        data_length: int = 10
 
     # Set up the sockets (until one exists)
     while True:
@@ -34,8 +42,8 @@ def main (ip: str = "192.168.1.112", use_cv: bool = False):
         model = Model()
         model.load("../../models/sign_model")
 
-    # Most recent letters
-    recents: list = [None] * 10
+    # Most recent results
+    recents: list = [None] * data_length
 
     # Loop forever
     while True:
@@ -45,8 +53,11 @@ def main (ip: str = "192.168.1.112", use_cv: bool = False):
             try:
                 image = Image("../../data/camera.jpg")
                 break
+
+            # If there is an issue loading image, send a none
             except:
                 time.sleep(0.001)
+                client.send("None".encode())
 
         # Make the prediction
         prediction = model.predict(image)
@@ -57,10 +68,10 @@ def main (ip: str = "192.168.1.112", use_cv: bool = False):
         recents = [recents[i - 1] for i in range(1, len(recents))]
         recents.insert(0, prediction)
 
-        # Determine the letter
+        # Determine the result
         result: str = determine_result(recents)
 
-        # Check if the letter is valid
+        # Check if the result is valid
         if result != None:
             print("Current Prediction: %s" % result)
         else:
@@ -73,23 +84,23 @@ def main (ip: str = "192.168.1.112", use_cv: bool = False):
             pass
 
         # Wait some time
-        time.sleep(0.1)
+        time.sleep(0.02)
 
 
 def determine_result (recents: "list[PredictionData]") -> str:
     '''
-    Attempts to determine the letter most likely to be
+    Attempts to determine the result most likely to be
     based on a list of predictions
     '''
 
-    # Get the number of letters of the same type
+    # Get the number of results of the same type
     check: str = recents[0].result
     count: int = 1
     for prediction in recents[1:]:
         if prediction != None and prediction.result == check:
             count += 1
     
-    # If the number of letters is not consistent, then return None
+    # If the number of results is not consistent, then return None
     if float(count) / float(len(recents)) < 0.95:
         return None
 
@@ -109,4 +120,9 @@ def determine_result (recents: "list[PredictionData]") -> str:
 
 # When the main loop is called
 if __name__ == "__main__":
-    main(use_cv=True)
+
+    # Specify the type of network used
+    use_cv: bool = True if len(sys.argv) > 1 and sys.argv[1] == "opencv" else False
+
+    # Call the main loop
+    main(use_cv=use_cv)
