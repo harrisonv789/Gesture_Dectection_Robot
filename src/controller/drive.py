5f072a97c2b5dde98c7183737bbb33685d431602
harrisonv789 <harrison.v1@live.com>
2022-10-12 22:52:37 +1100

    Updated the drive code. The rover now consumes too much power :(

--- src/controller/drive.py
+++ src/controller/drive.py
@@ -11,20 +11,15 @@ class Driver:
     '''
     
     # Define the outputs for the motors
-    LEFT_MOTOR_GPIO: int = 16 # Pin 16, GPIO23
-    RIGHT_MOTOR_GPIO: int = 11 # Pin 11, GPIO17
+    LEFT_MOTOR: int = 0
+    RIGHT_MOTOR: int = 1
     
-    # Store the pinouts for the PCA device
-    PWMA = 0
-    AIN1 = 1
-    PWMB = 5
-    AIN2 = 2
-    BIN1 = 3
-    BIN2 = 4
+    # Define the speed multiplier
+    MULTIPLIER: float = 0.3
 
-    def __init__ (self):
+    def __init__ (self) -> None:
         '''
-        The consrtructor sets up the GPIO connection
+        The constructor sets up the GPIO connection
         and allows for sending commands to the output
         pins.
         '''
@@ -32,52 +27,85 @@ class Driver:
         # Set up the PWM
         self.pwm = PCA9685(0x40, debug=False)
         self.pwm.setPWMFreq(50)
+
+        print("Initialised PWM for motor controls.")
+        self.stop()
+
+
+    def __get_pwm_pin__ (self, motor: int) -> int:
+        '''
+        Returns the ID for the pinout of the PWM
+        associated with this particular motor.
+        '''
+        return 0 if motor == self.LEFT_MOTOR else 5
+    
+    
+    def __get_input_pins__ (self, motor: int) -> tuple[int]:
+        '''
+        Returns the pair of input pinouts depending on
+        the motor that is returned to be commanded.
+        '''
+        if motor == self.LEFT_MOTOR:
+            return (1, 2)
+        else:
+            return (3, 4)
+    
         
-        # Initialise all the pins
-        #gpio.setmode(gpio.BOARD)
-        #gpio.setwarnings(False)
-        #gpio.setup(self.LEFT_MOTOR_GPIO, gpio.OUT)
-        #gpio.setup(self.RIGHT_MOTOR_GPIO, gpio.OUT)
-        print("Initialised GPIO pins for motor controls.")
-        
-        
-    def drive (self, speed: float, steer: float = 0.0):
+    def drive (self, speed: float, steer: float = 0.0) -> None:
         '''
         Drives the motor forward in some direction,
         with a steering factor that has -1.0 to move
         left and 1.0 to move right.
         '''
-        self.motor_send(self.LEFT_MOTOR_GPIO, 0.5)#speed if steer <= 0.0 else speed - steer)
-        #self.motor_send(self.RIGHT_MOTOR_GPIO, speed if steer >= 0.0 else speed + steer)
+        
+        # Determine the new speed
+        speed *= self.MULTIPLIER
+        
+        # Send both motors
+        self.motor_send(self.LEFT_MOTOR, speed)
+        self.motor_send(self.RIGHT_MOTOR, speed)
         
         
-    def motor_send (self, motor: int, power: float = 1.0):
+    def motor_send (self, motor: int, power: float = 1.0) -> None:
         '''
         Moves a motor with a particular amount of power
         based on the power value passed through.
         '''
+        
+        # Determines the duty cycle from 0 to 100
         duty: int = int(abs(power) * 100)
-        self.pwm.setDutycycle(self.PWMA, duty)
-        print(abs(power) * 100.0)
+        
+        # Get the appropriate pins
+        pwm_pin: int = self.__get_pwm_pin__(motor)
+        input_pins: tuple[int] = self.__get_input_pins__(motor)
+        
+        # Update the duty cycle of the PWM
+        self.pwm.setDutycycle(pwm_pin, duty)
+        
+        # Positive spinning
         if power > 0.0:
-            self.pwm.setLevel(self.AIN1, 0)
-            self.pwm.setLevel(self.AIN2, 1)
+            self.pwm.setLevel(input_pins[0], 0)
+            self.pwm.setLevel(input_pins[1], 1)
+        
+        # Negative spinning
         elif power < 0.0:
-            self.pwm.setLevel(self.AIN1, 1)
-            self.pwm.setLevel(self.AIN2, 0)
+            self.pwm.setLevel(input_pins[0], 1)
+            self.pwm.setLevel(input_pins[1], 0)
+        
+        # Send zeroes
         else:
-            self.pwm.setDutycycle(self.PWMA, 0)
+            self.pwm.setDutycycle(pwm_pin, 0)
 
         print("Driver :: Sending Motor %d at power %s" % (motor, power))
     
     
-    def stop (self):
+    def stop (self) -> None:
         '''
         Stops all motors and stops the robot from driving
         in any direction.
         '''
-        self.motor_send(self.LEFT_MOTOR_GPIO, 0)
-        self.motor_send(self.RIGHT_MOTOR_GPIO, 0)
+        self.motor_send(self.LEFT_MOTOR, 0)
+        self.motor_send(self.RIGHT_MOTOR, 0)
         print("Driver :: Stop Action.")
     
         
